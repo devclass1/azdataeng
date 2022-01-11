848299a258aa31612069911eb83281fb58f2e5fa
devclass1 <72622124+devclass1@users.noreply.github.com>
2022-01-11 19:58:34 +0530

    Create createlogtable.sql

---
+++ createlogtable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [serverlogs]
+(
+[remote_addr] varchar(20),
+[time_local] varchar(100),
+[request] varchar(200),
+[status] int,
+[bytes] int,
+[remote_user] varchar(100),
+[http_user_agent] varchar(500)
+)
