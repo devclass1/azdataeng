3977a7311adea1fdbc7a5b7145553140f646c6f3
devclass1 <72622124+devclass1@users.noreply.github.com>
2022-04-26 10:40:47 +0530

    Update dedicatedpool1.sql

--- dedicatedpool1.sql
+++ dedicatedpool1.sql
@@ -7,4 +7,35 @@ WITH
   IDENTITY = 'devrajclass2',
   SECRET = '5+Z5pgRQSdDl9gJVkstZ3dBRKKPL3MSqAPBVOFdeXCYt7yGnRBa0o0xH3KUdCj1wuThfW5zppBZxi3syC9dHiQ==';
   
+  CREATE EXTERNAL DATA SOURCE log_data
+WITH (    LOCATION   = 'abfss://data@devrajclass2.dfs.core.windows.net',
+          CREDENTIAL = AzureStorageCredential,
+          TYPE = HADOOP
+)
   
+CREATE EXTERNAL FILE FORMAT TextFileFormat WITH (  
+      FORMAT_TYPE = DELIMITEDTEXT,  
+    FORMAT_OPTIONS (  
+        FIELD_TERMINATOR = ',',
+        FIRST_ROW = 2))
+        
+ 
+ CREATE EXTERNAL TABLE [logdata]
+(
+    [Id] [int] NULL,
+	[Correlationid] [varchar](200) NULL,
+	[Operationname] [varchar](200) NULL,
+	[Status] [varchar](100) NULL,
+	[Eventcategory] [varchar](100) NULL,
+	[Level] [varchar](100) NULL,
+	[Time] [datetime] NULL,
+	[Subscription] [varchar](200) NULL,
+	[Eventinitiatedby] [varchar](1000) NULL,
+	[Resourcetype] [varchar](1000) NULL,
+	[Resourcegroup] [varchar](1000) NULL
+)
+WITH (
+ LOCATION = '/input/Log.csv',
+    DATA_SOURCE = log_data,  
+    FILE_FORMAT = TextFileFormat
+)
