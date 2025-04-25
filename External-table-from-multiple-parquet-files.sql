f550cca60ee452823019d4667d0b38048cc2ec24
devclass1 <72622124+devclass1@users.noreply.github.com>
2025-04-25 22:55:04 +0530

    Create External-table-from-multiple-parquet-files.sql

---
+++ External-table-from-multiple-parquet-files.sql
@@ -0,0 +1,29 @@
+-- Step 1: Create External Data Source
+CREATE EXTERNAL DATA SOURCE AzureBlobStorageLogs
+WITH (
+    TYPE = HADOOP,
+    LOCATION = 'https://sbstorage3045.blob.core.windows.net/sbcontainer/input/input_ac/'
+);
+
+-- Step 2: Create External File Format for Parquet
+CREATE EXTERNAL FILE FORMAT ParquetFileFormat
+WITH (
+    FORMAT_TYPE = PARQUET
+);
+
+-- Step 3: Create External Table
+CREATE EXTERNAL TABLE dbo.ActivityLogsExternal
+(
+    [resourceId] NVARCHAR(500),
+    [time] DATETIME2,
+    [operationName] NVARCHAR(200),
+    [category] NVARCHAR(200),
+    [resultType] NVARCHAR(100),
+    [level] NVARCHAR(100),
+    [properties] NVARCHAR(MAX)
+)
+WITH (
+    LOCATION = '/',
+    DATA_SOURCE = AzureBlobStorageLogs,
+    FILE_FORMAT = ParquetFileFormat
+);
